e6191320800d4fcacd9f133658d5769172d1d53d
A.R.Wheeldon2 <b6048857@eeehaydn.ncl.ac.uk>
2017-07-29 18:13:38 +0100

    config: add faraday

---
+++ loadlib_faraday.mk
@@ -0,0 +1,7 @@
+### DEFAULT LIB SPEC
+export LIB_ROOT=/eda/dk/Faraday/L90_SP_RVT/fsd0a_a/2009Q2v2.0/GENERIC_CORE_1D0V/FrontEnd
+export LIB_V=1v
+export LIB_OPT=
+export LIB_STACKED=
+export LIB_TEMP=25c
+LIB_VERILOG=$(LIB_ROOT)/verilog/fse0a_d_generic_core_30.lib.src
